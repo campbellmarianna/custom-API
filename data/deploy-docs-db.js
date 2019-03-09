0788a7c3dd58232559066d3af5994b3c6552bf6c
Marianna <mckd2018@gmail.com>
2019-03-08 17:23:13 -0800

    add mongoose to db connection updates

--- data/deploy-docs-db.js
+++ data/deploy-docs-db.js
@@ -2,18 +2,23 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-const url = "mongodb://localhost/deploy-docs-db";
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  url,
-  { useNewUrlParser: true },
-  function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to database");
+// const url = "mongodb://localhost/deploy-docs-db";
+// mongoose.Promise = global.Promise;
+// mongoose.connect(
+  // url,
+  // { useNewUrlParser: true },
+  // function(err, db) {
+  //   assert.equal(null, err);
+  //   console.log("Connected successfully to database");
 
     // db.close(); turn on for testing
-  }
-);
+  // }
+// );
+
+// connect Mongoose to your DB
+// var mongoose = require(‘mongoose’);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deploy-docs-db');
+
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
 mongoose.set("debug", true);
 
